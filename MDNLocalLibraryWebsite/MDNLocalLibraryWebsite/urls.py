631366d34f977774d6ee0d9de22b35fd2e0d8f64
Sean O'Connor <status101@live.co.uk>
2018-02-04 13:41:41 +1300

    fixed typo

--- MDNLocalLibraryWebsite/MDNLocalLibraryWebsite/urls.py
+++ MDNLocalLibraryWebsite/MDNLocalLibraryWebsite/urls.py
@@ -32,6 +32,6 @@ urlpatterns = [
 
 # Serve static files in debug mode only
 from django.conf import settings
-if settings.DEBUGor True:
+if settings.DEBUG or True:
 	from django.conf.urls.static import static
 	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
